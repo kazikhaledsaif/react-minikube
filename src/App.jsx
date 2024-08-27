d13d3b99bcee80dd4efc6fb6a1258a0062c89b11
Kazi Khaled Saif <kksaif05@gmail.com>
2024-08-27 18:00:26 +0900

    feat: change to see argocd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -13,14 +10,6 @@ function App() {
         </a>
       </div>
       <h1>Testing React app in mini kube</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
